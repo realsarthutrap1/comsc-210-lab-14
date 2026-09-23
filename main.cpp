8f50c2e0359c401830cfc89e527820b0732db02e
sarthucodesagain <261507123+realsarthutrap1@users.noreply.github.com>
2026-09-23 07:48:11 -0700

    lab14: add color class

--- main.cpp
+++ main.cpp
@@ -2,6 +2,22 @@
 #include <iostream>
 using namespace std;
 
+class Color {
+private:
+    int red;
+    int green;
+    int blue;
+
+public:
+    void setRed(int value);
+    void setGreen(int value);
+    void setBlue(int value);
+    int getRed() const;
+    int getGreen() const;
+    int getBlue() const;
+    void print() const;
+};
+
 int main() {
     return 0;
 }
